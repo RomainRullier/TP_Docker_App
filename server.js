e19260bc7fe032650448e7ea9ec4227443b7f020
Romain Rullier <71820670+RomainRullier@users.noreply.github.com>
2023-07-28 11:38:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8085
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Docker'))
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
